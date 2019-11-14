06b67fb7b021e1caed1bd6249b85325faffef371
kang <kaspi6000@gmail.com>
2019-11-14 12:40:51 +0900

    마크다운 라이브러리 삭제

--- front-end/src/component/Write/index.tsx
+++ front-end/src/component/Write/index.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import ReactMarkDown from "react-markdown";
-// import htmlParser from 'react-markdown/plugins/html-parser';
 import Markdown from "markdown-to-jsx";
 
 const WriteWrap = styled.div`
